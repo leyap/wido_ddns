1321ff28d5a5158761b5f56b5a50616ecf081141
leyap <leyapin@gmail.com>
2014-08-19 20:09:58 +0800

    new

---
+++ wido_ddns.ino
@@ -0,0 +1,160 @@
+//ddns for arduino
+//by lisper <lisper.li@dfrobot.com>
+//GPL 3.0
+
+#include <Adafruit_CC3000.h>
+#include <SPI.h>
+#include "utility/debug.h"
+#include "utility/socket.h"
+
+// These are the interrupt and control pins
+#define ADAFRUIT_CC3000_IRQ   7  // MUST be an interrupt pin!
+// These can be any two pins
+#define ADAFRUIT_CC3000_VBAT  5
+#define ADAFRUIT_CC3000_CS    10
+// Use hardware SPI for the remaining pins
+// On an UNO, SCK = 13, MISO = 12, and MOSI = 11
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
+		SPI_CLOCK_DIVIDER); // you can change this clock speed
+
+#define WLAN_SSID       "DFRobot-Internal"           // cannot be longer than 32 characters!
+#define WLAN_PASS       "zwrobot2014"
+// Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
+#define WLAN_SECURITY   WLAN_SEC_WPA2
+
+#define LISTEN_PORT           80   // What TCP port to listen on for connections.
+
+//Adafruit_CC3000_Server webServer(LISTEN_PORT);
+uint32_t timeout;
+char checkipData[20];
+uint32_t ddnsip;
+
+
+void setup(void) {
+	pinMode (13, OUTPUT);
+	pinMode (12, OUTPUT);
+	Serial.begin(115200);
+	Serial.println(F("Hello, CC3000!\n")); 
+	//while (!Serial);
+	//Serial.println ("Input any key to start:");
+	//while (!Serial.available ());
+	Serial.print("Free RAM: "); 
+	Serial.println(getFreeRam(), DEC);
+
+	/* Initialise the module */
+	Serial.println(F("\nInitializing..."));
+	if (!cc3000.begin()) {
+		Serial.println(F("Couldn't begin()! Check your wiring?"));
+		while(1);
+	}
+
+	Serial.print(F("\nAttempting to connect to ")); 
+	Serial.println(WLAN_SSID);
+	if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+		Serial.println(F("Failed!"));
+		while(1);
+	}
+
+	Serial.println(F("Connected!"));
+
+	Serial.println(F("Request DHCP"));
+	while (!cc3000.checkDHCP()) {
+		delay(100); // ToDo: Insert a DHCP timeout!
+	}  
+
+	/* Display the IP address DNS, Gateway, etc. */
+	while (! displayConnectionDetails()) {
+		delay(1000);
+	}
+
+	/*********************************************************/
+	/* You can safely remove this to save some flash memory! */
+	/*********************************************************/
+
+
+	// Start listening for connections
+	//webServer.begin();
+
+	while (ddnsip == 0) {
+		if (! cc3000.getHostByName("nic.changeip.com", &ddnsip)) {
+			Serial.println(F("Couldn't resolve!"));
+		}
+		delay(500);
+	}
+	cc3000.printIPdotsRev(ddnsip);
+
+	Serial.println(F("Listening for connections..."));
+}
+
+
+
+//
+void loop(void) {
+	///////////////////////////////////////////////////////
+	if (millis () - timeout > 10000) {
+		timeout = millis ();  
+		Serial.println ("checkIP");
+		uint32_t checkip = cc3000.IP2U32 (209,208,4,56);
+		//uint32_t checkip = cc3000.IP2U32 (192,168,0,9);
+		Adafruit_CC3000_Client checkClient = cc3000.connectTCP (checkip, 80);
+		if (checkClient.connected ()) {
+			checkClient.fastrprintln ("GET /");
+			checkClient.fastrprintln ("");
+			//        delay (1000);  
+			while (!checkClient.available() && millis () - timeout < 2000);
+			if (checkClient.available () > 0) {
+				char checkipBuffer[20];
+				checkClient.read (checkipBuffer, 20, 0);
+				checkClient.close();
+				char *sub = strchr (checkipBuffer, '\n');
+				if (sub)
+					*sub = '\0';
+				Serial.println (checkipBuffer);
+				if (strcmp (checkipData, checkipBuffer) != 0) {
+					Serial.println ("ip is changed");
+					Adafruit_CC3000_Client ddnsClient = cc3000.connectTCP (ddnsip, 80);     
+					strcpy (checkipData, checkipBuffer);
+					if (ddnsClient.connected()) {
+						Serial.print ("connected ddns");
+						ddnsClient.fastrprintln ("GET /nic/update?u=jshield&p=dfrobot2014&hostname=outlet.ddns.info");
+						uint32_t ddnstimeout = millis ();
+						while (!ddnsClient.available() && millis () - ddnstimeout < 2000);
+						while (ddnsClient.available ()) {
+							char data = ddnsClient.read ();
+							Serial.print (data);
+						}
+						Serial.println ("===");
+					}
+					ddnsClient.close();
+				}
+
+			}
+		}
+	}
+
+	/////////////////////////////////////////////////////////
+}
+
+bool displayConnectionDetails(void) {
+	uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
+
+	if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv)) {
+		Serial.println(F("Unable to retrieve the IP Address!\r\n"));
+		return false;
+	} 
+	else {
+		Serial.print(F("\nIP Addr: ")); 
+		cc3000.printIPdotsRev(ipAddress);
+		Serial.print(F("\nNetmask: ")); 
+		cc3000.printIPdotsRev(netmask);
+		Serial.print(F("\nGateway: ")); 
+		cc3000.printIPdotsRev(gateway);
+		Serial.print(F("\nDHCPsrv: ")); 
+		cc3000.printIPdotsRev(dhcpserv);
+		Serial.print(F("\nDNSserv: ")); 
+		cc3000.printIPdotsRev(dnsserv);
+		Serial.println();
+		return true;
+	}
+}
+
